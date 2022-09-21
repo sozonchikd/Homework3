e705b18e20eebfb3daa62f54990171ae87c206d6
Sozonchik <den.sozon@mail.ru>
2022-09-21 18:38:32 +0500

    Сделал задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+byte pizza = 121;
+System.out.println("Значение переменной pizza с типом byte равно " + pizza);
+short temperature = -48;
+System.out.println("Значение переменной temperature с типом short равно " + temperature);
+int computer = 7777;
+System.out.println("Значение переменной computer с типом int равно " + computer);
+long money = 21775900;
+System.out.println("Значение переменной money с типом long равно " + money);
+double forceOfGravity = 9.80665;
+System.out.println("Значение переменной forceOfGravity с типом double равно " + forceOfGravity);
+float cable = 1.6f;
+System.out.println("Значение переменной cable с типом float равно " + cable);
 
     }
 }
