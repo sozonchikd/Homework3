fbfd0eef357c7665beef47888b2d981e3e499611
Sozonchik <den.sozon@mail.ru>
2022-09-22 13:38:30 +0500

    Сделал задачу 2

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,37 @@ short temperature = -48;
 System.out.println("Значение переменной temperature с типом short равно " + temperature);
 int computer = 7777;
 System.out.println("Значение переменной computer с типом int равно " + computer);
-long money = 21775900;
+long money = 21775900L;
 System.out.println("Значение переменной money с типом long равно " + money);
 double forceOfGravity = 9.80665;
 System.out.println("Значение переменной forceOfGravity с типом double равно " + forceOfGravity);
 float cable = 1.6f;
 System.out.println("Значение переменной cable с типом float равно " + cable);
+
+System.out.println("Задача 2");
+float flour = 27.12f;
+System.out.println(flour);
+long brick = 987678965549L;
+System.out.println(brick);
+float sugarWeight = 2.786f;
+System.out.println(sugarWeight);
+short voltage = 220;
+System.out.println(voltage);
+boolean nominalVoltage = voltage > 300;
+System.out.println(nominalVoltage);
+short tire = 569;
+System.out.println(tire);
+short accountOnTheCard = -159;
+System.out.println(accountOnTheCard);
+short metalWeight = 27897;
+System.out.println(metalWeight);
+byte automobile = 67;
+System.out.println(automobile);
+
+
+
+
+
 
     }
 }
